6ea6a60db0a128c12f316703eaad00d4a4026087
Teo <75574673+sunsakis@users.noreply.github.com>
2024-07-28 13:48:56 +0300

    responsive background

--- pages/index.js
+++ pages/index.js
@@ -39,32 +39,42 @@ export default function Home({ imgHashes, newestPrice, newestCounter, messages, 
   const [message, setMessage] = useState(newestMessage);
   const [counter, setCounter] = useState(0);
   const [msgPrices, setPrices] = useState(newestPrice);
-  const [imgHash, setImgHash] = useState(imgHashes[newestCounter - 1]);
+  const [imgHash, setImgHash] = useState(imgHashes[0]);
 
-  function getImgURLFromHash(_imgHash) {
-    return _imgHash.replace('ipfs://', 'https://ipfs.io/ipfs/');
+
+    // Step 2: Modify getImgURLFromHash to handle undefined inputs
+  function getImgURLFromHash(imgHash) {
+    if (imgHash === '' || imgHash === undefined) {
+      // Return a default image URL or handle the error as appropriate
+      // return 'defaultImageUrl';
+      console.log('imgHash is undefined');
+      return;
+    }
+    // Existing logic to generate the image URL from the hash
+    return imgHash.replace('ipfs://', 'https://ipfs.io/ipfs/');
   }
 
   const updateStyle = (backgroundImageUrl) => {
-    const screenWidth = window.innerWidth;
-    const screenHeight = window.innerHeight;
-    const minDimension = Math.min(screenWidth, screenHeight);
-
-    // Define maximum sizes for the gradients
-    const maxFirstGradientSize = 300; // Example: 300px
-    const maxSecondGradientSize = 600; // Example: 600px
-
-    // Dynamically adjust the base size of the gradients based on the smaller screen dimension
-    // Ensure it does not exceed the maximum sizes
-    const baseSize = Math.min(minDimension * 0.25, maxFirstGradientSize);
-    const firstGradient = `${baseSize}px`;
-    const secondGradient = `${Math.min(baseSize * 2, maxSecondGradientSize)}px`;
+    const viewportWidth = window.innerWidth;
+    const viewportHeight = window.innerHeight;
+  
+    // Use the smaller of the viewport dimensions to influence the spotlight size, ensuring it works for both landscape and portrait
+    let spotlightSize = Math.min(viewportWidth, viewportHeight);
+  
+    // Cap the spotlight size at 1500px
+    spotlightSize = Math.min(spotlightSize, 1000);
+  
+    // Calculate the first and second gradients
+    // The first gradient is the transparent center of the spotlight
+    // The second gradient begins the dimming effect, so it should be slightly larger than the first
+    const firstGradient = `${spotlightSize / 2.8}px`; // Half the size for a circular gradient
+    const secondGradient = `${spotlightSize / 1.8}px`; // Full size for the dimming effect to start
     
     setStyle({
       backgroundImage: `linear-gradient(rgba(0, 0, 0, 0.1), rgba(0, 0, 0, 0.1)), radial-gradient(circle at center, transparent ${firstGradient}, black ${secondGradient}), url(${backgroundImageUrl})`,
-      backgroundPosition: 'center, center',
-      backgroundSize: 'auto, contain',
-      backgroundRepeat: 'no-repeat, no-repeat',
+      backgroundPosition: 'center',
+      backgroundSize: 'cover',
+      backgroundRepeat: 'no-repeat',
       height: '100vh',
       width: '100vw',
       position: 'absolute',
@@ -74,7 +84,7 @@ export default function Home({ imgHashes, newestPrice, newestCounter, messages, 
   };
 
   useEffect(() => {
-    
+
     const checkConnection = async () => {
       if (window.ethereum) {
         const provider = new ethers.providers.Web3Provider(window.ethereum, "any");
@@ -89,20 +99,19 @@ export default function Home({ imgHashes, newestPrice, newestCounter, messages, 
       }
     };
     const updateBackground = async () => {
-      // Ensure any required data is loaded before updating the background
-      // For example, if imgHashes or newestCounter are fetched asynchronously, wait for them to load
-      const newImgURL = getImgURLFromHash(imgHashes[newestCounter - 1]);
-      console.log(newImgURL); // Log newImgURL
-      updateStyle(newImgURL); // Call updateStyle with the new image URL
+      const newImgURL = getImgURLFromHash(imgHashes[0]);
+      updateStyle(newImgURL);
     };
-    updateBackground(); // Call on component mount
-  
-    updateBackground(); // Call on component mount
+
+    updateBackground(); 
+
+    const newImgURL = getImgURLFromHash(imgHash);
+    window.addEventListener('resize', updateStyle(newImgURL));
     window.ethereum?.on('chainChanged', checkConnection);
     window.ethereum?.on('accountsChanged', checkConnection);
+
     checkConnection();
-    const newImgURL = getImgURLFromHash(imgHashes[newestCounter - 1]);
-    window.addEventListener('resize', updateStyle(newImgURL));
+
 
     // Cleanup
     return () => {
@@ -118,31 +127,45 @@ export default function Home({ imgHashes, newestPrice, newestCounter, messages, 
     delta: { up: 100, down: 150 },
   });
 
-  const showPreviousMessage = () => {
+  const showNextMessage = () => {
     setCounter(prevCounter => {
-      const newCounter = Math.min(prevCounter + 1, newestCounter - 1);
+      const lastIndex = messages.length - 1;
+      let newCounter;
+      if (prevCounter === lastIndex) {
+        newCounter = 0;
+      } else {
+        newCounter = prevCounter - 1;
+      }
       const newImgHash = imgHashes[newCounter];
-      const newImgURL = getImgURLFromHash(newImgHash);
-      updateStyle(newImgURL);
       setMessage(messages[newCounter]);
       setPrices(prices[newCounter]);
       setImgHash(imgHashes[newCounter]);
-      console.log(newCounter); // Log newCounter
-      return newCounter; // Return the updated counter value
+      if (newImgHash !== undefined || newImgHash !== '') {
+        const newImgURL = getImgURLFromHash(newImgHash);
+        updateStyle(newImgURL);
+      } else {
+        console.log('imgHash is undefined');
+      }
+      console.log(newImgHash); 
+      return newCounter; 
     });
   };
   
-  const showNextMessage = () => {
+  const showPreviousMessage = () => {
     setCounter(prevCounter => {
-      if (prevCounter === 0) return prevCounter;
-      const newCounter = Math.max(prevCounter - 1, 0);
+      if (prevCounter >= newestCounter) return prevCounter;
+      const newCounter = prevCounter + 1;
       const newImgHash = imgHashes[newCounter];
-      const newImgURL = getImgURLFromHash(newImgHash);
-      updateStyle(newImgURL);
       setMessage(messages[newCounter]);
       setPrices(prices[newCounter]);
       setImgHash(imgHashes[newCounter]);
-      console.log(newCounter); // Log newCounter
+      if (newImgHash !== undefined || newImgHash !== '') {
+        const newImgURL = getImgURLFromHash(newImgHash);
+        updateStyle(newImgURL);
+      } else {
+        console.log('imgHash is undefined');
+      }
+      console.log(newImgHash); // Log newCounter
       return newCounter; // Return the updated counter value
     });
   };
@@ -152,6 +175,7 @@ export default function Home({ imgHashes, newestPrice, newestCounter, messages, 
       console.log('Resetting counter');
       setCounter(0);
       setMessage(messages[0]);
+      setImgHash(imgHashes[0]);
     } else {
       console.log('Counter is already at its default state');
     }
@@ -205,7 +229,7 @@ export async function getServerSideProps() {
     const message = await contract.getMessages(i);
     const price = await contract.getPrices(i);
     const imgHash = await contract.getImgHashes(i);
-    console.log(`message ${i} is ${imgHash}`);
+    console.log(`message nr.${i} is ${message} IMGHASH: ${imgHash}`);
     messages.push(message);
     prices.push(ethers.utils.formatEther(price));
     imgHashes.push(imgHash);
