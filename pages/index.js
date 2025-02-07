bf87e3c2c2e008a0f55d7fccbc2141636e95ad6e
Teo <75574673+sunsakis@users.noreply.github.com>
2025-02-07 16:17:42 +0200

    homepage shows RGCVII history

--- pages/index.js
+++ pages/index.js
@@ -4,11 +4,22 @@ import { useState, useEffect } from 'react';
 import Footer from '../components/footer.js';
 import Header from '../components/header.js';
 import Message from '../components/message.js';
-import ABI from '../contract/ABI.js';
+import { useSwipeable } from 'react-swipeable';
+import oldABI from '../contract/oldABI.js';
+import Image from 'next/image.js';
+import Link from 'next/link'
 
-export default function Home({ imgHash, price, message, settings, messenger }) {
+export default function Hamsterverse({ names = [], imgHashes = [], messages = [], prices = [], newestCounter = 0 }) {
   const [style, setStyle] = useState({});
+  const [message, setMessage] = useState(messages[0]);
+  const [imgHash, setImgHash] = useState(imgHashes[0]);
+  const [name, setName] = useState(names[0]);
+  const [price, setPrices] = useState(prices[0]);
   const [windowSize, setWindowSize] = useState({});
+  const [currentIndex, setCurrentIndex] = useState(newestCounter - 1);
+  const [counter, setCounter] = useState(0);
+  const [isLoading, setIsLoading] = useState(false);
+  const [allMessages, setAllMessages] = useState(messages);
 
   useEffect(() => {
     const updateBackground = async () => {
@@ -20,15 +31,12 @@ export default function Home({ imgHash, price, message, settings, messenger }) {
     };
   
     updateBackground(); 
-
-    const newImgURL = getImgURLFromHash(imgHash);
     window.addEventListener('resize', handleResize);
-
+  
     return () => {
       window.removeEventListener('resize', handleResize);
     };
-  }, [imgHash, windowSize]);
-
+  }, [imgHash]);
 
   function getImgURLFromHash(imgHash) {
     if (imgHash === '' || imgHash === undefined) {
@@ -40,24 +48,15 @@ export default function Home({ imgHash, price, message, settings, messenger }) {
   const updateStyle = (backgroundImageUrl) => {
     const viewportWidth = window.innerWidth;
     const viewportHeight = window.innerHeight;
-  
-    // Define a minimum size for the spotlight
-    const minSpotlightSize = 200; // Example minimum size
-
-    // Use the smaller of the viewport dimensions to influence the spotlight size
-    // Ensure the spotlight size does not go below the minimum size
+    const minSpotlightSize = 200;
     let spotlightSize = Math.max(Math.min(viewportWidth, viewportHeight) * 0.3, minSpotlightSize);
-  
-    // Calculate the first and second gradients
-    // The first gradient is the transparent center of the spotlight
-    // The second gradient begins the dimming effect, so it should be slightly larger than the first
-    const firstGradient = `${spotlightSize * 0.5}px`; // Half the size for a circular gradient
-    const secondGradient = `${spotlightSize}px`; // Full size for the dimming effect to start
+    const firstGradient = `${spotlightSize * 0.5}px`;
+    const secondGradient = `${spotlightSize}px`;
     
     setStyle({
       backgroundImage: `linear-gradient(rgba(0, 0, 0, 0.1), rgba(0, 0, 0, 0.2)), radial-gradient(circle at center, transparent ${firstGradient}, black ${secondGradient}), url(${backgroundImageUrl})`,
       backgroundPosition: 'center',
-      backgroundSize: window.innerWidth < 450 ? 'contain' : 'contain',
+      backgroundSize: window.innerWidth < 450 ? 'cover' : 'contain',
       backgroundRepeat: 'no-repeat',
       height: '100vh',
       width: '100vw',
@@ -67,96 +66,260 @@ export default function Home({ imgHash, price, message, settings, messenger }) {
     });
   };
 
+  const showNextMessage = async () => {
+    const newIndex = currentIndex + 1;
+    if (newIndex >= newestCounter - 1) {
+      // Fetch newest message
+      setIsLoading(true);
+      try {
+        const response = await fetch('/api/fetch-message', {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify({ index: newestCounter - 1 }), // Newest message index
+        });
+  
+        if (!response.ok) throw new Error('Network response was not ok');
+        const data = await response.json();
+  
+        setMessage(data.message);
+        setPrices(data.price);
+        setImgHash(data.imgHash);
+        setName(data.name);
+        setCurrentIndex(newestCounter - 1);
+        setCounter(0);
+      } catch (error) {
+        console.error("Error fetching newest message:", error);
+      } finally {
+        setIsLoading(false);
+      }
+    } else {
+      // Rest of the code remains the same
+      setIsLoading(true);
+      try {
+        const response = await fetch('/api/fetch-message', {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify({ index: newIndex }),
+        });
+  
+        if (!response.ok) throw new Error('Network response was not ok');
+        const data = await response.json();
+  
+        setMessage(data.message);
+        setPrices(data.price);
+        setImgHash(data.imgHash);
+        setName(data.name);
+        setCurrentIndex(newIndex);
+        setCounter(prevCounter => prevCounter - 1);
+      } catch (error) {
+        console.error("Error fetching next message:", error);
+      } finally {
+        setIsLoading(false);
+      }
+    }
+  };
+  
+  const showPreviousMessage = async () => {
+    const newIndex = currentIndex - 1;
+    if (newIndex >= 0) {
+      setIsLoading(true);
+      try {
+        const response = await fetch('/api/fetch-message', {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify({ index: newIndex }),
+        });
+  
+        if (!response.ok) throw new Error('Network response was not ok');
+        const data = await response.json();
+  
+        setMessage(data.message);
+        setPrices(data.price);
+        setImgHash(data.imgHash);
+        setName(data.name);
+        setCurrentIndex(newIndex);
+        setCounter(prevCounter => prevCounter + 1);
+      } catch (error) {
+        console.error("Error fetching previous message:", error);
+      } finally {
+        setIsLoading(false);
+      }
+    }
+  };
+
   return (
     <>
       <Head>
-        <title>Expensive Message</title>
-        <meta name="description" content="Free speech for profit." />
+        <title>Expensive Message History</title>
+        <meta name="description" content="An archive of Hamsterverse's most Expensive Messages." />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-
-        {/* OpenGraph tags */}
-        <meta property="og:title" content="Expensive Message" />
-        <meta property="og:description" content="Expensive Message - free speech for profit." />
+        <meta property="og:title" content="Archives" />
+        <meta property="og:description" content="An archive of Hamsterverse's most Expensive Messages." />
         <meta property="og:url" content="https://www.expensivemessage.com" /> 
         <meta property="og:site_name" content="Expensive Message" />
-        
-        {/* Frame metadata tags */}
-        <meta property="fc:frame" content="vNext" />
-        <meta property="fc:frame:image" content={getImgURLFromHash(imgHash)} />
-        <meta property="fc:frame:button:1" content="View Top $DEGEN" />
-        <meta property="fc:frame:button:1:action" content="post" />
-        <meta property="fc:frame:post_url" content="https://expensivemessage.com/api/frame/image/latest" />
       </Head>
-      <Header />
       <main>
-        <div style={style} className="w-full">
-          <div className="min-h-screen flex-col flex">
-            <div className="flex flex-grow justify-center items-center bg-black bg-opacity-10">
-              <Message text={message} messenger={messenger} />
+        <div style={style} className="w-full relative">
+          <div className="min-h-screen flex-col flex relative">
+            <Header />
+            <div className="w-full absolute top-28 items-center flex left-0 right-0 justify-between px-10 sm:px-20 md:px-32 lg:px-56 xl:px-96 z-10">
+              <div className="justify-start">
+                <button 
+                  className="text-3xl" 
+                  onClick={currentIndex > 0 ? showPreviousMessage : undefined}
+                  style={{ opacity: currentIndex > 0 ? 1 : 0.25 }}
+                  disabled={isLoading}
+                >
+                  <Image 
+                    src="arrowLeft.svg" 
+                    alt="Arrow to the left" 
+                    width={25} 
+                    height={25}
+                  />
+                </button>
+              </div>
+              <div className="justify-end">
+                <button 
+                  className="text-3xl" 
+                  onClick={currentIndex < newestCounter - 1 ? showNextMessage : undefined}
+                  style={{ opacity: currentIndex < newestCounter - 1 ? 1 : 0.25 }}
+                  disabled={isLoading}
+                >
+                  <Image 
+                    src="arrowRight.svg" 
+                    alt="Arrow to the right" 
+                    width={25} 
+                    height={25}
+                  />
+                </button>
+              </div>
             </div>
-            <Footer settings={settings} msgPrices={price} text={"This"} />
+            <div className="flex flex-grow justify-center items-center mb-28 bg-black bg-opacity-20">
+              <Message text={message} messenger={name} />
+            </div>
+            <div className=" bottom-3 left-0 right-0 z-20">
+              <div className="sm:m-5">
+                <p className="text-sm text-right tracking-tight mr-9 text-gray-300">
+                  This message cost
+                </p>
+                <p className="text-right mr-8">
+                  <b className="text-2xl">
+                    {price + " "}
+                    <Link 
+                      href="https://app.uniswap.org/explore/tokens/base/0x11dc980faf34a1d082ae8a6a883db3a950a3c6e8"
+                      className="text-green-500 hover:text-green-400 transition duration-500 ease-in-out hover:underline"
+                      rel="nofollow" 
+                      target="_blank"
+                    >
+                      RGCVII
+                    </Link>
+                  </b>
+                </p>
+              </div>
+            </div>
+          </div>
+          <div className="w-full absolute top-28 items-center flex left-0 right-0 justify-between px-10 sm:px-20 md:px-32 lg:px-56 xl:px-96">
+            <div className="justify-start">
+              <button 
+                className="text-3xl" 
+                onClick={counter > 0 ? showNextMessage : undefined}
+                style={{ opacity: counter > 0 ? 1 : 0.25 }}
+                disabled={isLoading}
+              >
+                <Image 
+                  src="arrowLeft.svg" 
+                  alt="Arrow to the left" 
+                  width={25} 
+                  height={25}
+                />
+              </button>
+            </div>
+            <div className="justify-end">
+              <button 
+                className="text-3xl" 
+                onClick={counter < allMessages.length - 1 ? showPreviousMessage : undefined}
+                style={{ opacity: counter < allMessages.length - 1 ? 1 : 0.25 }}
+                disabled={isLoading}
+              >
+                <Image 
+                  src="arrowRight.svg" 
+                  alt="Arrow to the right" 
+                  width={25} 
+                  height={25}
+                />
+              </button>
+            </div>
           </div>
         </div>
       </main>
     </>
-  )
+  );
 }
 
 export async function getServerSideProps() {
   try {
-
-    // Provider for Base network
     const baseProvider = new ethers.providers.JsonRpcProvider(`https://base-mainnet.g.alchemy.com/v2/${process.env.ALCHEMY_API}`);
-    const contract = new ethers.Contract(process.env.NEXT_PUBLIC_CONTRACT_ADDRESS_FRONTPAGE, ABI, baseProvider);
-
-    // Provider for Ethereum mainnet (for ENS resolution)
+    const contract = new ethers.Contract(process.env.NEXT_PUBLIC_CONTRACT_ADDRESS, oldABI, baseProvider);
     const mainnetProvider = new ethers.providers.JsonRpcProvider(`https://eth-mainnet.g.alchemy.com/v2/${process.env.ALCHEMY_API}`);
 
-    let newestMessage, newestCounter, newestPrice, newestImgHash, newestMessenger;
+    const newMessageCall = await contract.readMessage();
+    const newestCounter = newMessageCall[1].toNumber();
+    const newestPrice = await contract.getPrice();
+    const formatPrice = ethers.utils.formatEther(newestPrice);
 
-    try {
-      const newMessageCall = await contract.readMessage();
-      newestMessage = newMessageCall[0];
-      newestMessenger = newMessageCall[1];
-      newestImgHash = newMessageCall[3];
-      newestPrice = newMessageCall[4];
-      //newestName = newMessageCall[5];
-      //timestamp = newMessageCall[6];
-      
-    } catch (error) {
-      console.error("Error calling readMessage:", error);
-      newestMessage = "Error reading message";
-    }
+    let message = '', price = '0', imgHash = '', resolvedName = '';
+    
+    if (newestCounter > 0) {
+      const i = newestCounter - 1;
+      const [msg, prc, img, messenger] = await Promise.all([
+        contract.getMessages(i),
+        contract.getPrices(i),
+        contract.getImgHashes(i),
+        contract.getMessengers(i),
+      ]);
 
-    const formatPrice = parseFloat(ethers.utils.formatEther(newestPrice)).toString();
+      message = msg;
+      price = prc;
+      imgHash = img;
+      resolvedName = messenger;
 
-    // Attempt to resolve ENS name using the mainnet provider
-    try {
-      const ensName = await mainnetProvider.lookupAddress(newestMessenger);
-      if (ensName) {
-        newestMessenger = ensName;
+      try {
+        const ensName = await mainnetProvider.lookupAddress(messenger);
+        if (ensName) {
+          resolvedName = ensName;
+        }
+      } catch (error) {
+        console.error("Error resolving ENS:", error);
       }
-    } catch (ensError) {
-      console.error("Error resolving ENS:", ensError);
-      // If ENS resolution fails, we'll keep the original address
     }
 
     return {
       props: {
-        price: formatPrice,
-        message: newestMessage,
-        imgHash: newestImgHash,
-        messenger: newestMessenger,
-        networkName: "BASE_MAINNET",
+        newestPrice: formatPrice,
+        newestCounter: newestCounter,
+        messages: message ? [message] : [],
+        prices: price ? [parseFloat(ethers.utils.formatEther(price)).toString()] : [],
+        imgHashes: imgHash ? [imgHash] : [],
+        names: resolvedName ? [resolvedName] : [],
       },
     };
   } catch (error) {
     console.error("Error in getServerSideProps:", error);
     return {
       props: {
-        error: "Failed to fetch data"
-      }
+        newestPrice: '0',
+        newestCounter: 0,
+        messages: [],
+        prices: [],
+        imgHashes: [],
+        names: [],
+      },
     };
   }
 }
-  
